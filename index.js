8e5625eb40d659f333c63437d00a6a2c3e242ab6
Manuel Castro <manuel.castro.ofc@gmail.com>
2023-04-19 13:06:05 -0300

    Actividad 2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo.")
+const cumpelaños = new Date(2000, 11, 9)
+
+let datosPersonales = [
+    "Manuel",
+    22,
+    true,
+    cumpelaños,
+    libroFavorito = {
+        titulo: "La teoria del todo",
+        autor: "Stephen Hawking",
+        publicado: 2002,
+        link: "https://dariososafoula.files.wordpress.com/2017/01/hawking_-stephen-la-teoria-del-todo.pdf"
+    }
+
+];
+
+console.log(datosPersonales)

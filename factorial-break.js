5eaa818b905f1fc43c4e9859b9b8367db3880885
Manuel Castro <manuel.castro.ofc@gmail.com>
2023-04-24 13:28:00 -0300

    Actividades de bucles

---
+++ factorial-break.js
@@ -0,0 +1,11 @@
+let factorial = 1;
+let i = 1;
+
+while (true) {
+    factorial += 1;
+    i++;
+    if (i > 10) {
+        break
+    }
+    console.log(factorial)
+}
---
+++ factorial-while.js
@@ -0,0 +1,8 @@
+let factorial = 0;
+let i = 1;
+
+while (i <= 10) {
+    factorial += 1;
+    i++;
+    console.log(factorial)
+}

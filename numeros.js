487e2782334438ce9ed8ddc46a2f198f6f132465
Manuel Castro <manuel.castro.ofc@gmail.com>
2023-04-24 22:46:10 -0300

    Actividad con muneros

---
+++ numeros.js
@@ -0,0 +1,17 @@
+let alturaEnCentimetro = 176;
+console.log(alturaEnCentimetro)
+
+let alturaEnMetros = 1.76;
+console.log(alturaEnMetros)
+
+let peso = 54.5;
+console.log(peso)
+
+let miAlturaArriba = Math.ceil(alturaEnMetros);
+console.log(miAlturaArriba)
+
+let miAlturaAbajo = Math.floor(alturaEnMetros);
+console.log(miAlturaAbajo)
+
+let esIgual = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+console.log(esIgual)
